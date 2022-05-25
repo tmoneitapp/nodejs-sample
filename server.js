69c569e6e63be5ab162dbb88dd56185d64302022
tmoneitapp <47148947+tmoneitapp@users.noreply.github.com>
2022-05-25 19:04:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-
+console.log('MongoURL : %s', mongoURL);
 if (mongoURL == null) {
   var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
   // If using plane old env vars via service discovery
